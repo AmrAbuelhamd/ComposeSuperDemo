7c5b7e1de3b709911c2134ce496e9dcf3840faeb
Amr Mohamed <amohamed@zennex.ru>
2021-09-06 16:44:44 +0700

    Added improvements.

--- app/src/main/java/com/blogspot/soyamr/composesuperdemo/MainActivity.kt
+++ app/src/main/java/com/blogspot/soyamr/composesuperdemo/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.blogspot.soyamr.composesuperdemo
 
+import android.content.Intent
 import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -13,10 +14,13 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -27,37 +31,127 @@ import androidx.compose.ui.unit.dp
 import com.blogspot.soyamr.composesuperdemo.ui.theme.ComposeSuperDemoTheme
 
 class MainActivity : ComponentActivity() {
-
-    val sampleData = listOf(
-        Message("he", "texttexttexttexttexttexttexttext"),
-        Message("she", "text text text text text text text "),
-        Message("it", "texxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxt"),
-        Message("we", "texttexttexttexttexttexttexttexttexttexttexttexttexttexttexttext"),
-        Message(
-            "are",
-            "texttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttext"
-        ),
-        Message("you", "text"),
-    )
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // defines the activity's layout
         setContent {
-            ComposeSuperDemoTheme {
+            MyApp {
                 Conversation(
                     messages = sampleData
                 )
             }
         }
+        startActivity(Intent(this, ScaffoldActivity::class.java))
     }
+
 }
+
+val sampleData = listOf(
+    Message("he", "texttexttexttexttexttexttexttext"),
+    Message("she", "text text text text text text text "),
+    Message("it", "texxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxt"),
+    Message("we", "texttexttexttexttexttexttexttexttexttexttexttexttexttexttexttext"),
+    Message(
+        "are",
+        "texttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttext"
+    ),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+    Message("you", "text"),
+)
 
 data class Message(val author: String, val message: String)
 
+// What if you want to create a container that has all the common configurations of your app?
 @Composable
-fun MessageCard(msg: Message) {
-    Row(modifier = Modifier.padding(all = 8.dp)) {
+fun MyApp(content: @Composable () -> Unit) {
+    ComposeSuperDemoTheme {
+        content()
+    }
+}
+
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    var count by remember { mutableStateOf(0) }
+    Column(modifier = Modifier.fillMaxHeight()) {
+        LazyColumn(modifier = Modifier.weight(1f)) {
+            items(messages) { message ->
+                MessageCard(msg = message, count) { newCount ->
+                    count = newCount
+                }//state hoisting: State hoisting is the way to make internal state controllable by the function that called it.
+                Divider(color = Color.White)
+            }
+        }
+        Divider(color = Color.Black)
+        Counter(count)
+    }
+}
+
+@Composable
+fun MessageCard(msg: Message, count: Int, updateCount: (Int) -> Unit) {
+    // We keep track if the message is expanded or not in this variable
+    var isExpanded by remember { mutableStateOf(false) }
+    val surfaceColo: Color by animateColorAsState(
+        if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface
+    )
+
+    Row(
+        modifier = Modifier
+            .padding(all = 8.dp)
+            .clip(RoundedCornerShape(4.dp))
+            .clickable(onClick = {})
+            .padding(16.dp)
+            .fillMaxWidth()
+    ) {
         Image(
             painter = painterResource(id = R.drawable.amira),
             contentDescription = "contact profile picture",
@@ -69,12 +163,7 @@ fun MessageCard(msg: Message) {
 
         Spacer(modifier = Modifier.width(8.dp))
 
-        // We keep track if the message is expanded or not in this variable
-        var isExpanded by remember { mutableStateOf(false) }
-        val surfaceColo: Color by animateColorAsState(
-            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface
-        )
-        Column {
+        Column(modifier = Modifier.align(Alignment.CenterVertically)) {
             Text(
                 text = msg.author,
                 color = MaterialTheme.colors.secondaryVariant,
@@ -95,7 +184,10 @@ fun MessageCard(msg: Message) {
                 Text(
                     text = msg.message,
                     modifier = Modifier
-                        .clickable { isExpanded = !isExpanded }
+                        .clickable {
+                            isExpanded = !isExpanded
+                            updateCount(count + 1)
+                        }
                         .padding(all = 4.dp),
                     // If the message is expanded, we display all its content
                     // otherwise we only display the first line
@@ -109,32 +201,21 @@ fun MessageCard(msg: Message) {
 }
 
 @Composable
-fun Conversation(messages: List<Message>) {
-    LazyColumn {
-        items(messages) { message ->
-            MessageCard(msg = message)
-        }
-    }
+fun Counter(count: Int) {
+    Text(text = "$count times", color = if (count % 2 == 0) Color.Green else Color.White)
 }
 
 @Preview(
-    uiMode = Configuration.UI_MODE_NIGHT_NO
+    uiMode = Configuration.UI_MODE_NIGHT_NO,
+    showBackground = true
 )
 @Composable
 fun PreviewConversation() {
-    ComposeSuperDemoTheme {
-        Conversation(
-            messages = listOf(
-                Message("he", "texttexttexttexttexttexttexttext"),
-                Message("she", "text text text text text text text "),
-                Message("it", "texxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxt"),
-                Message("we", "texttexttexttexttexttexttexttexttexttexttexttexttexttexttexttext"),
-                Message(
-                    "are",
-                    "texttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttext"
-                ),
-                Message("you", "text"),
+    ComposeSuperDemoTheme(darkTheme = false) {
+        MyApp {
+            Conversation(
+                messages = sampleData
             )
-        )
+        }
     }
 }
---
+++ app/src/main/java/com/blogspot/soyamr/composesuperdemo/ScaffoldActivity.kt
@@ -0,0 +1,61 @@
+package com.blogspot.soyamr.composesuperdemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.blogspot.soyamr.composesuperdemo.ui.theme.ComposeSuperDemoTheme
+
+class ScaffoldActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApp {
+                LayoutsCodelab()
+            }
+        }
+    }
+}
+
+@Composable
+fun LayoutsCodelab() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "LayoutsCodelab")
+                },
+                actions = {
+                    IconButton(onClick = { /* doSomething() */ }) {
+                        Icon(Icons.Filled.Favorite, contentDescription = null)
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        BodyContent(Modifier.padding(innerPadding))
+    }
+}
+
+@Composable
+fun BodyContent(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Text(text = "Hi there!")
+        Text(text = "Thanks for going through the Layouts codelab")
+    }
+}
+
+@Preview
+@Composable
+fun LayoutsCodelabPreview() {
+    ComposeSuperDemoTheme() {
+        LayoutsCodelab()
+    }
+}
